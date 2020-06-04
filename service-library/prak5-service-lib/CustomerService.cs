e479a63d84bb2262c1248a00fc02ca4259acb10c
noudadrichem <info@noudadrichem.com>
2020-06-04 15:19:54 +0200

    init new solutions

---
+++ service-library/prak5-service-lib/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace prak5_service_lib {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CustomerService" in both code and config file together.
+    public class CustomerService : ICustomerService {
+        public customerSet loginCustomer(string username, string password) {
+            using (prac5_dbEntities db = new prac5_dbEntities()) {
+                var query = db.customerSets.Where(c => c.username == username && c.password == password).FirstOrDefault<customerSet>();
+                return query;
+            }
+        }
+
+        public customerSet registerCustomer(string newUsername, string newPassword) {
+            // TODO check if use exists
+            var newCustomer = new customerSet {
+                username = newUsername,
+                password = newPassword,
+                balance = 20
+            };
+
+            using (prac5_dbEntities db = new prac5_dbEntities()) {
+                return db.customerSets.Add(newCustomer);  
+            }
+        }
+    }
+}
---
+++ wpf-client/practicum_5/Register.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PasswordGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practicum_5
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Button_Click(object sender, RoutedEventArgs e)
+        {
+            password.Text = generatePassword();
+            MessageBox.Show("Vergeet niet het wachtwoord!");
+        }
+
+        private void To_Login(object sender, RoutedEventArgs e)
+        {   
+            Login LoginWindow= new Login();
+            LoginWindow.Show();
+            this.Close();
+        }
+
+        private string generatePassword() {
+            var pwd = new Password(16).IncludeLowercase().IncludeUppercase().IncludeSpecial();
+            return pwd.Next().ToString();
+        }
+    }
+}
