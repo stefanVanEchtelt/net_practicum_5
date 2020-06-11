f12f4e363d7722c3cd923ca201a5f039de8349b6
noudadrichem <info@noudadrichem.com>
2020-06-11 23:16:03 +0200

    rename

---
+++ javafx-client-mvn/src/main/java/app/controllers/Start.java
@@ -0,0 +1,22 @@
+package app.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Webshop");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
